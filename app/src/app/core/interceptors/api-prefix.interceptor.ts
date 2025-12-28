dee571e11af2a03b59c48ee11cef1598bd1d8009
jawahaartheella <jawahaartheella@gmail.com>
2025-12-28 11:56:39 +0530

    added api service

---
+++ app/src/app/core/interceptors/api-prefix.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+export const apiPrefixInterceptor: HttpInterceptorFn = (req, next) => {
+  // If URL already starts with http, leave it (for external APIs)
+  if(req.url.startsWith('http')) {
+    return next(req);
+  }
+
+  const apiReq = req.clone({
+    url: `${environment.apiBaseUrl}${req.url}`
+  });
+
+  console.log('Interceptor hit:', req.url);
+
+  return next(apiReq);
+};
