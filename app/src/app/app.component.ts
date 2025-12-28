de53cb694d10c6ff31459957d9b7376fd044c1ee
jawahaartheella <jawahaartheella@gmail.com>
2025-12-28 11:57:08 +0530

    added routing

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { FiltersComponent } from './components/filters/filters.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet, 
-    NavbarComponent,
-    DashboardComponent,
-    FiltersComponent
+    NavbarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
