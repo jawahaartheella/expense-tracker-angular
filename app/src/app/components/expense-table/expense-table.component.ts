5b21224d2403e60966f23690700784efe3e1bd8b
jawahaartheella <jawahaartheella@gmail.com>
2026-01-03 11:54:47 +0530

    Added functionality for expense table

--- app/src/app/components/expense-table/expense-table.component.ts
+++ app/src/app/components/expense-table/expense-table.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
-import { Expense } from '../../core/models/expense.model';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgClass } from '@angular/common';
+
+import { Expense } from '../../core/models/expense.model';
+import { Category } from '../../core/models/category.model';
 
 @Component({
   selector: 'app-expense-table',
@@ -9,7 +11,35 @@ import { NgClass } from '@angular/common';
   styleUrl: './expense-table.component.css'
 })
 export class ExpenseTableComponent {
-  @Input() showNotesOptions: boolean = false;
   @Input() expenseData?: Array<Expense>;
+  @Input() categories?: Array<Category>;
+  @Input() showNotesOptions: boolean = false;
   @Input() isTableFixedLayout: boolean = false;
+
+  @Output() emitExpense = new EventEmitter<Expense>;
+  @Output() openEditCanvas = new EventEmitter<Expense>;
+  @Output() deleteExpense = new EventEmitter<Expense>;
+  @Output() bookmarkExpense = new EventEmitter<Expense>;
+
+  emitUpdatedExpense(event: any) {
+    let updatedExpense = event;
+    this.emitExpense.emit(updatedExpense);
+  }
+
+  getCatgeoryName(id: string) {
+    const categoryId: number = parseInt(id);
+    return this.categories?.find(c => c.id == categoryId)?.name;
+  }
+
+  openEditExpeseCanvas(expense: Expense) {
+    this.openEditCanvas.emit(expense);
+  }
+
+  emitExpenseToDelete(expense: Expense) {
+    this.deleteExpense.emit(expense);
+  }
+
+  emitExpenseToBookmark(expense: Expense) {
+    this.bookmarkExpense.emit(expense);
+  }
 }
