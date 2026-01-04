58616362e8610125f55b3bc77a6815a32b718954
jawahaartheella <jawahaartheella@gmail.com>
2026-01-04 13:25:18 +0530

    Applied pagination for expenses table

--- app/src/app/components/expense-table/expense-table.component.ts
+++ app/src/app/components/expense-table/expense-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
 import { NgClass } from '@angular/common';
 
 import { Expense } from '../../core/models/expense.model';
@@ -10,16 +10,57 @@ import { Category } from '../../core/models/category.model';
   templateUrl: './expense-table.component.html',
   styleUrl: './expense-table.component.css'
 })
-export class ExpenseTableComponent {
-  @Input() expenseData?: Array<Expense>;
+export class ExpenseTableComponent implements OnInit, OnChanges{
+  @Input() expenseData: Array<Expense> = [];
   @Input() categories?: Array<Category>;
   @Input() showNotesOptions: boolean = false;
   @Input() isTableFixedLayout: boolean = false;
+  @Input() enablePagination: boolean = false;
 
   @Output() emitExpense = new EventEmitter<Expense>;
   @Output() openEditCanvas = new EventEmitter<Expense>;
   @Output() deleteExpense = new EventEmitter<Expense>;
   @Output() bookmarkExpense = new EventEmitter<Expense>;
+
+  paginatedExpenses: Array<Expense> = [];
+  pageSize!: number;
+  startCount!: number;
+  endCount!: number;
+  totalCount!:number;
+
+  ngOnInit() {
+    this.paginatedExpenses = this.expenseData;
+    if(this.enablePagination) {
+      this.initPagination();
+    }
+  }
+
+  ngOnChanges() {
+    if(this.enablePagination) {
+      this.initPagination();
+    }
+  }
+
+  initPagination() {
+    this.totalCount = this.expenseData?.length;
+    this.pageSize = this.expenseData?.length >= 10 ? 10 : this.expenseData?.length;
+    this.startCount = 0;
+    this.endCount = this.expenseData?.length ? this.pageSize : 0;
+    this.paginatedExpenses = this.expenseData?.slice(this.startCount, this.endCount);
+  }
+
+  loadPreviousExpenses() {
+    this.startCount = this.startCount - this.pageSize;
+    this.endCount = this.startCount + this.pageSize;
+    this.paginatedExpenses = this.expenseData?.slice(this.startCount, this.endCount);
+  }
+
+  loadNextExpenses() {
+    this.startCount = this.startCount + this.pageSize;
+    const nextCount = this.endCount + this.pageSize;
+    this.endCount = !(nextCount > this.totalCount) ? nextCount : this.expenseData.length;
+    this.paginatedExpenses = this.expenseData?.slice(this.startCount, this.endCount);
+  }
 
   emitUpdatedExpense(event: any) {
     let updatedExpense = event;
